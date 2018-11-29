45b3fb0bb309bebbe1abc0c90ca3c3637e235def
Evan Slaton <evanslaton@gmail.com>
2018-11-29 10:28:08 -0800

    Added a test test

--- basicLibrary/linter/src/test/java/AppTest.java
+++ basicLibrary/linter/src/test/java/AppTest.java
@@ -5,8 +5,14 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
+    @Test public void testPrintMaxMinAbsentValues() {
         App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+                int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        assertNotNull("app should have a greeting", classUnderTest.getUniqueValues(weeklyMonthTemperatures));
     }
 }
